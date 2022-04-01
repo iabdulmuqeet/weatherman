cc8c18b7d742ff48f0f514324c1ff3a37eaa29ca
Abdul Muqeet Arshad <abdulmuqeet.arshad@devsinc.com>
2022-04-01 16:07:24 +0500

    added error messages for invalid year

--- weatherman.rb
+++ weatherman.rb
@@ -6,9 +6,9 @@ months = %w[Jan Fed Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
 
 case ARGV[0]
 when '-e'
-  max_temp_hash = { date: '', max_temperature: 0 }
-  min_temp_hash = { date: '', min_temperature: 100 }
-  max_humidity_hash = { date: '', max_humidity: 0 }
+  max_temp_hash = {}
+  min_temp_hash = {}
+  max_humidity_hash = {}
 
   Dir.foreach(ARGV[2]) do |filename|
     if filename.split('_')[2] == ARGV[1]
@@ -19,6 +19,12 @@ when '-e'
           max_temperature = l.split(',')[1].to_i
           min_temperature = l.split(',')[3].to_i
           max_humidity = l.split(',')[7].to_i
+
+          if max_temp_hash[:max_temperature].nil?
+            max_temp_hash = { date: date, max_temperature: max_temperature }
+            min_temp_hash = { date: date, min_temperature: min_temperature }
+            max_humidity_hash = { date: date, max_humidity: max_humidity }
+          end
 
           if max_temperature >= max_temp_hash[:max_temperature]
             max_temp_hash = { date: date, max_temperature: max_temperature }
@@ -35,18 +41,23 @@ when '-e'
       end
     end
   end
-  max_temp_month = max_temp_hash[:date].split('-')[1].to_i - 1
-  max_temp_date = max_temp_hash[:date].split('-')[2].to_i
+  if max_temp_hash[:max_temperature]
 
-  min_temp_month = min_temp_hash[:date].split('-')[1].to_i - 1
-  min_temp_date = min_temp_hash[:date].split('-')[2].to_i
+    max_temp_month = max_temp_hash[:date].split('-')[1].to_i - 1
+    max_temp_date = max_temp_hash[:date].split('-')[2].to_i
 
-  max_humidity_month = max_humidity_hash[:date].split('-')[1].to_i - 1
-  max_humidity_date = max_humidity_hash[:date].split('-')[2].to_i
+    min_temp_month = min_temp_hash[:date].split('-')[1].to_i - 1
+    min_temp_date = min_temp_hash[:date].split('-')[2].to_i
 
-  puts "Highest: #{max_temp_hash[:max_temperature]}C on #{months[max_temp_month]} #{max_temp_date}"
-  puts "Lowest: #{min_temp_hash[:min_temperature]}C on #{months[min_temp_month]} #{min_temp_date}"
-  puts "Humid: #{max_humidity_hash[:max_humidity]}% on #{months[max_humidity_month]} #{max_humidity_date}"
+    max_humidity_month = max_humidity_hash[:date].split('-')[1].to_i - 1
+    max_humidity_date = max_humidity_hash[:date].split('-')[2].to_i
+
+    puts "Highest: #{max_temp_hash[:max_temperature]}C on #{months[max_temp_month]} #{max_temp_date}"
+    puts "Lowest: #{min_temp_hash[:min_temperature]}C on #{months[min_temp_month]} #{min_temp_date}"
+    puts "Humid: #{max_humidity_hash[:max_humidity]}% on #{months[max_humidity_month]} #{max_humidity_date}"
+  else
+    puts 'Oopps! No record found for this year.'
+  end
 
 when '-a'
   highest_avg = []
@@ -79,7 +90,7 @@ when '-c'
 
   Dir.open(ARGV[2]) do |_filename|
     File.open("#{ARGV[2]}/#{ARGV[2]}_#{year}_#{month}.txt") do |line|
-      line.seek(390)
+      line.seek(391)
       line.readlines.each do |l|
         temperatures << { highest: l.split(',')[1].to_i, lowest: l.split(',')[3].to_i }
       end
@@ -87,7 +98,7 @@ when '-c'
   end
 
   puts "#{month} #{year}"
-  temperatures.each_with_index do |temp, i|
+  temperatures[0, (temperatures.size - 1)].each_with_index do |temp, i|
     # Simple Task Output
 
     # print "\n#{i + 1} "
@@ -100,5 +111,9 @@ when '-c'
     print "\n#{i + 1} "
     print('+'.blue * temp[:lowest] + '+'.red * temp[:highest] + " #{temp[:lowest]}C - #{temp[:highest]}C")
   end
+  puts "\n"
+
+else
+  puts 'Invalid Command!'
 
 end
